5f8479385725c1b4d029b3e3385d376131198246
MehdiNoui <mehdinoui2004@gmail.com>
2025-11-09 23:13:25 +0100

    Add Morel Mushroom Block

---
+++ src/main/java/net/mehdinoui/fungidelight/common/block/mushrooms/MorelBlock.java
@@ -0,0 +1,22 @@
+package net.mehdinoui.fungidelight.common.block.mushrooms;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.MushroomBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class MorelBlock extends MushroomBlock {
+    protected static final VoxelShape SHAPE = Block.box(5.0D, 0.0D, 5.0D, 11.0D, 13.0D, 11.0D);
+
+    public MorelBlock(Properties pProperties) {
+        super(pProperties, null);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return SHAPE;
+    }
+}
