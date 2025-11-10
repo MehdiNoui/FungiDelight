c2845d761c18a60e1c8d9a1108be3f5a70f2d683
MehdiNoui <mehdinoui2004@gmail.com>
2025-11-10 15:59:42 +0100

    Workaround: Make InkyCap mushroom convert to colony on rich soil (by randomTick override)

--- src/main/java/net/mehdinoui/fungidelight/common/block/mushrooms/MorelBlock.java
+++ src/main/java/net/mehdinoui/fungidelight/common/block/mushrooms/MorelBlock.java
@@ -1,12 +1,16 @@
 package net.mehdinoui.fungidelight.common.block.mushrooms;
 
+import net.mehdinoui.fungidelight.common.registry.ModBlocks;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.MushroomBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import vectorwing.farmersdelight.common.tag.ModTags;
 
 public class MorelBlock extends MushroomBlock {
     protected static final VoxelShape SHAPE = Block.box(5.0D, 0.0D, 5.0D, 11.0D, 13.0D, 11.0D);
@@ -18,5 +22,13 @@ public class MorelBlock extends MushroomBlock {
     @Override
     public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
         return SHAPE;
+    }
+    public void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        super.randomTick(state, level, pos, random);
+        if (level.getBlockState(pos.below()).is(ModTags.MUSHROOM_COLONY_GROWABLE_ON)) {
+            if (random.nextInt(4) == 0) {
+                level.setBlock(pos, ModBlocks.MOREL_MUSHROOM_COLONY.get().defaultBlockState(), 3);
+            }
+        }
     }
 }
