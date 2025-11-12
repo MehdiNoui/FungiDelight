e74930a45fa54d74a226eee04374af23a874bed4
MehdiNoui <mehdinoui2004@gmail.com>
2025-11-12 21:38:02 +0100

    PROTOTYPE: implement custom Huge Morel Feature

--- src/main/java/net/mehdinoui/fungidelight/common/block/mushrooms/MorelMushroomBlock.java
+++ src/main/java/net/mehdinoui/fungidelight/common/block/mushrooms/MorelMushroomBlock.java
@@ -12,17 +12,20 @@ import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import vectorwing.farmersdelight.common.tag.ModTags;
 
-public class MorelBlock extends MushroomBlock {
+import static net.mehdinoui.fungidelight.common.registry.ModConfiguredFeatures.HUGE_MOREL_MUSHROOM;
+
+public class MorelMushroomBlock extends MushroomBlock {
     protected static final VoxelShape SHAPE = Block.box(5.0D, 0.0D, 5.0D, 11.0D, 13.0D, 11.0D);
 
-    public MorelBlock(Properties pProperties) {
-        super(pProperties, null);
+    public MorelMushroomBlock(Properties pProperties) {
+        super(pProperties, HUGE_MOREL_MUSHROOM);
     }
 
     @Override
     public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
         return SHAPE;
     }
+    @Override
     public void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
         super.randomTick(state, level, pos, random);
         if (level.getBlockState(pos.below()).is(ModTags.MUSHROOM_COLONY_GROWABLE_ON)) {
