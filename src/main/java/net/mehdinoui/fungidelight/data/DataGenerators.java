aede4da9b1b53e5401ef71bc80150dcb2ecfa192
MehdiNoui <mehdinoui2004@gmail.com>
2025-11-12 23:13:26 +0100

    add configured features provider

--- src/main/java/net/mehdinoui/fungidelight/data/DataGenerators.java
+++ src/main/java/net/mehdinoui/fungidelight/data/DataGenerators.java
@@ -22,5 +22,7 @@ public class DataGenerators {
         // Model
         generator.addProvider(event.includeClient(), new ModItemModelProvider(packOutput, existingFileHelper));
         generator.addProvider(event.includeClient(), new ModBlockStateProvider(packOutput, existingFileHelper));
+        // World-gen
+        generator.addProvider(event.includeServer(), new ModWorldGenProvider(packOutput, lookupProvider));
     }
 }
