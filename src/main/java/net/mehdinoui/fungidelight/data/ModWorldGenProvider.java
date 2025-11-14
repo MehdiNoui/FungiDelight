eb509357901803928cb8a4f3d9e9814c7db835ef
MehdiNoui <mehdinoui2004@gmail.com>
2025-11-14 12:06:32 +0100

    hook new worldgen providers (placedFeatures and BiomeModifier)

--- src/main/java/net/mehdinoui/fungidelight/data/ModWorldGenProvider.java
+++ src/main/java/net/mehdinoui/fungidelight/data/ModWorldGenProvider.java
@@ -1,19 +1,24 @@
 package net.mehdinoui.fungidelight.data;
 
 import net.mehdinoui.fungidelight.FungiDelight;
+import net.mehdinoui.fungidelight.common.registry.ModBiomeModifiers;
 import net.mehdinoui.fungidelight.common.registry.ModConfiguredFeatures;
+import net.mehdinoui.fungidelight.common.registry.ModPlacedFeatures;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 public class ModWorldGenProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
-            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap);
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(ForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
 
     public ModWorldGenProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries,BUILDER, Set.of(FungiDelight.MOD_ID));
