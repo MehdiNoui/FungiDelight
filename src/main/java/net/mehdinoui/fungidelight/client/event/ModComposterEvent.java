e47169aa396fb200f7f22f328f7644c21dd04372
MehdiNoui <mehdinoui2004@gmail.com>
2025-11-16 22:25:36 +0100

    init and add composter handler

---
+++ src/main/java/net/mehdinoui/fungidelight/client/event/ModComposterEvent.java
@@ -0,0 +1,29 @@
+package net.mehdinoui.fungidelight.client.event;
+
+import net.mehdinoui.fungidelight.FungiDelight;
+import net.mehdinoui.fungidelight.common.registry.ModItems;
+import net.minecraft.world.level.block.ComposterBlock;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+@Mod.EventBusSubscriber(modid = FungiDelight.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModComposterEvent {
+    @SubscribeEvent
+    public static void onCommonSetup(FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+            // 65%
+            ComposterBlock.COMPOSTABLES.put(ModItems.INKY_CAP_MUSHROOM.get(), 0.65f);
+            ComposterBlock.COMPOSTABLES.put(ModItems.MOREL_MUSHROOM.get(), 0.65f);
+            ComposterBlock.COMPOSTABLES.put(ModItems.INKY_CAP_MUSHROOM_STEM.get(), 0.65f);
+            ComposterBlock.COMPOSTABLES.put(ModItems.MOREL_MUSHROOM_STEM.get(), 0.65f);
+            // 85%
+            ComposterBlock.COMPOSTABLES.put(ModItems.INKY_CAP_MUSHROOM_BLOCK.get(), 0.85f);
+            ComposterBlock.COMPOSTABLES.put(ModItems.INKY_CAP_MUSHROOM_EDGE.get(), 0.85f);
+            ComposterBlock.COMPOSTABLES.put(ModItems.MOREL_MUSHROOM_BLOCK.get(), 0.85f);
+            // 100%
+            ComposterBlock.COMPOSTABLES.put(ModItems.INKY_CAP_MUSHROOM_COLONY.get(), 1.0f);
+            ComposterBlock.COMPOSTABLES.put(ModItems.MOREL_MUSHROOM_COLONY.get(), 1.0f);
+        });
+    }
+}
