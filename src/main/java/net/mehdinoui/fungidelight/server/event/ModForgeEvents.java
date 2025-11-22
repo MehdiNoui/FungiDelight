a6faebd70992c4f71c6c906f8d29e1bc35317f3d
MehdiNoui <mehdinoui2004@gmail.com>
2025-11-22 17:37:43 +0100

    improve config check

--- src/main/java/net/mehdinoui/fungidelight/server/event/ModForgeEvents.java
+++ src/main/java/net/mehdinoui/fungidelight/server/event/ModForgeEvents.java
@@ -19,42 +19,44 @@ public class ModForgeEvents {
     @SubscribeEvent
     public static void addCustomGoals(EntityJoinLevelEvent event) {
         // --- PIG LOGIC ---
-        if (event.getEntity() instanceof Pig pig) {
-            if (pig.goalSelector.getAvailableGoals().stream().anyMatch(
-                    wrappedGoal -> wrappedGoal.getGoal() instanceof TruffleDiggingGoal)) {
-                return;
-            }
-            if (Configuration.ENABLE_PIG_DIGGING.get()) {
+        if (Configuration.ENABLE_PIG_DIGGING.get()) {
+            if (event.getEntity() instanceof Pig pig) {
+                if (pig.goalSelector.getAvailableGoals().stream().anyMatch(
+                        wrappedGoal -> wrappedGoal.getGoal() instanceof TruffleDiggingGoal)) {
+                    return;
+                }
                 pig.goalSelector.addGoal(5, new TruffleDiggingGoal(pig));
             }
         }
         // --- WOLF LOGIC ---
-        if (event.getEntity() instanceof Wolf wolf) {
-            if (wolf.goalSelector.getAvailableGoals().stream().anyMatch(
-                    wrappedGoal -> wrappedGoal.getGoal() instanceof DogTruffleHuntGoal)) {
-                return;
-            }
-            if (Configuration.ENABLE_WOLF_HUNT_TRUFFLE.get()) {
+        if (Configuration.ENABLE_WOLF_HUNT_TRUFFLE.get()) {
+            if (event.getEntity() instanceof Wolf wolf) {
+                if (wolf.goalSelector.getAvailableGoals().stream().anyMatch(
+                        wrappedGoal -> wrappedGoal.getGoal() instanceof DogTruffleHuntGoal)) {
+                    return;
+                }
                 wolf.goalSelector.addGoal(2, new DogTruffleHuntGoal(wolf));
             }
         }
     }
     @SubscribeEvent
     public static void onLivingTick(LivingEvent.LivingTickEvent event) {
-        if (!event.getEntity().level().isClientSide && event.getEntity() instanceof Wolf wolf) {
-            // Check for Hunting Tag
-            if (wolf.getPersistentData().contains(ModInteractionEvents.HUNTING_TAG)) {
-                long endTime = wolf.getPersistentData().getLong(ModInteractionEvents.HUNTING_TAG);
-                // Particles Indicator that the dog is in truffle hunting mode
-                if (wolf.level().getGameTime() < endTime) {
-                    ServerLevel serverLevel = (ServerLevel) wolf.level();
-                    if (wolf.tickCount % 4 == 0) {
-                        serverLevel.sendParticles(ParticleTypes.MYCELIUM,
-                                wolf.getX(), wolf.getY() + 0.5, wolf.getZ(),
-                                3,
-                                0.3, 0.3, 0.3,
-                                0.05
-                        );
+        if (Configuration.ENABLE_WOLF_HUNT_TRUFFLE.get()) {
+            if (!event.getEntity().level().isClientSide && event.getEntity() instanceof Wolf wolf) {
+                // Check for Hunting Tag
+                if (wolf.getPersistentData().contains(ModInteractionEvents.HUNTING_TAG)) {
+                    long endTime = wolf.getPersistentData().getLong(ModInteractionEvents.HUNTING_TAG);
+                    // Particles Indicator that the dog is in truffle hunting mode
+                    if (wolf.level().getGameTime() < endTime) {
+                        ServerLevel serverLevel = (ServerLevel) wolf.level();
+                        if (wolf.tickCount % 4 == 0) {
+                            serverLevel.sendParticles(ParticleTypes.MYCELIUM,
+                                    wolf.getX(), wolf.getY() + 0.5, wolf.getZ(),
+                                    3,
+                                    0.3, 0.3, 0.3,
+                                    0.05
+                            );
+                        }
                     }
                 }
             }
