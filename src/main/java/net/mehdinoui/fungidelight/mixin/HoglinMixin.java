5efffcd4a179b6495f372a65513d507ed8a3f83f
MehdiNoui <mehdinoui2004@gmail.com>
2025-11-26 13:23:52 +0100

    allow Hoglins to eat Truffles and configure Mixin environment

---
+++ src/main/java/net/mehdinoui/fungidelight/mixin/HoglinMixin.java
@@ -0,0 +1,20 @@
+package net.mehdinoui.fungidelight.mixin;
+
+import net.mehdinoui.fungidelight.common.registry.ModItems;
+import net.minecraft.world.entity.monster.hoglin.Hoglin;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Hoglin.class)
+public class HoglinMixin {
+    @Inject(method = "isFood", at = @At("HEAD"), cancellable = true)
+    private void fungiDelight$isFood(ItemStack pStack, CallbackInfoReturnable<Boolean> cir) {
+        System.out.println("Mixin is checking food: " + pStack.getItem());
+        if (pStack.is(ModItems.TRUFFLE.get())) {
+            cir.setReturnValue(true);
+        }
+    }
+}
