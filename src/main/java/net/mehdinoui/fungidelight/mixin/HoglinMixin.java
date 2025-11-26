3de7fc541115e2c4f13398068df93c67872ee9b6
MehdiNoui <mehdinoui2004@gmail.com>
2025-11-26 23:34:48 +0100

    remove debug output

--- src/main/java/net/mehdinoui/fungidelight/mixin/HoglinMixin.java
+++ src/main/java/net/mehdinoui/fungidelight/mixin/HoglinMixin.java
@@ -12,7 +12,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class HoglinMixin {
     @Inject(method = "isFood", at = @At("HEAD"), cancellable = true)
     private void fungiDelight$isFood(ItemStack pStack, CallbackInfoReturnable<Boolean> cir) {
-        System.out.println("Mixin is checking food: " + pStack.getItem());
         if (pStack.is(ModItems.TRUFFLE.get())) {
             cir.setReturnValue(true);
         }
