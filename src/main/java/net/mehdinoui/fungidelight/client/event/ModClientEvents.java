b7286a8c76284a97f001c556dcb2e27bb64e0cbb
MehdiNoui <mehdinoui2004@gmail.com>
2025-11-27 00:15:53 +0100

    Add Cow Rendered with custom mushroom placer

--- src/main/java/net/mehdinoui/fungidelight/client/event/ModClientEvents.java
+++ src/main/java/net/mehdinoui/fungidelight/client/event/ModClientEvents.java
@@ -1,4 +1,17 @@
 package net.mehdinoui.fungidelight.client.event;
 
+import net.mehdinoui.fungidelight.FungiDelight;
+import net.mehdinoui.fungidelight.client.renderer.FDMCowRenderer;
+import net.mehdinoui.fungidelight.common.registry.ModEntities;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = FungiDelight.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModClientEvents {
+    @SubscribeEvent
+    public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(ModEntities.INKY_CAP_COW.get(), FDMCowRenderer::new);
+        event.registerEntityRenderer(ModEntities.MOREL_COW.get(), FDMCowRenderer::new);
+    }
 }
