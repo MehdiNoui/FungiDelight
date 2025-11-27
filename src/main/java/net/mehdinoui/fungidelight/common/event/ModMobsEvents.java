ab8af9fa8de6772cfceb8a7a24c5ad51f56b6af9
MehdiNoui <mehdinoui2004@gmail.com>
2025-11-27 11:16:33 +0100

    add SpawnPlacements event

--- src/main/java/net/mehdinoui/fungidelight/common/event/ModMobsEvents.java
+++ src/main/java/net/mehdinoui/fungidelight/common/event/ModMobsEvents.java
@@ -4,12 +4,17 @@ import net.mehdinoui.fungidelight.Configuration;
 import net.mehdinoui.fungidelight.FungiDelight;
 import net.mehdinoui.fungidelight.common.registry.ModEntities;
 import net.mehdinoui.fungidelight.common.registry.ModItems;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.SpawnPlacements;
 import net.minecraft.world.entity.animal.Cow;
+import net.minecraft.world.entity.animal.MushroomCow;
 import net.minecraft.world.entity.animal.Pig;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.common.crafting.CompoundIngredient;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -37,5 +42,25 @@ public class ModMobsEvents {
     public static void registerAttributes(EntityAttributeCreationEvent event) {
         event.put(ModEntities.INKY_CAP_COW.get(), Cow.createAttributes().build());
         event.put(ModEntities.MOREL_COW.get(), Cow.createAttributes().build());
+    }
+    @SubscribeEvent
+    public static void registerSpawnPlacements(SpawnPlacementRegisterEvent event) {
+         event.register(
+                ModEntities.INKY_CAP_COW.get(),
+                SpawnPlacements.Type.ON_GROUND,
+                Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+                (entityType, level, spawnType, pos, random) ->
+                        MushroomCow.checkMushroomSpawnRules((EntityType<MushroomCow>) (Object) entityType, level, spawnType, pos, random),
+                SpawnPlacementRegisterEvent.Operation.REPLACE
+        );
+
+        event.register(
+                ModEntities.MOREL_COW.get(),
+                SpawnPlacements.Type.ON_GROUND,
+                Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+                (entityType, level, spawnType, pos, random) ->
+                        MushroomCow.checkMushroomSpawnRules((EntityType<MushroomCow>) (Object) entityType, level, spawnType, pos, random),
+                SpawnPlacementRegisterEvent.Operation.REPLACE
+        );
     }
 }
