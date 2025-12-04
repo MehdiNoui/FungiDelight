636bebb78f776d2b428a2d1c0304e32fc0eb7170
MehdiNoui <mehdinoui2004@gmail.com>
2025-12-04 11:02:49 +0100

    add potion tooltip that matches the vanilla one

---
+++ src/main/java/net/mehdinoui/fungidelight/client/event/ModClientForgeEvents.java
@@ -0,0 +1,61 @@
+package net.mehdinoui.fungidelight.client.event;
+
+import net.mehdinoui.fungidelight.FungiDelight;
+import net.mehdinoui.fungidelight.common.registry.ModEffects;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
+
+@Mod.EventBusSubscriber(modid = FungiDelight.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class ModClientForgeEvents {
+
+    @SubscribeEvent
+    public static void onTooltip(ItemTooltipEvent event) {
+        ItemStack stack = event.getItemStack();
+        if (stack.getItem() != Items.POTION && stack.getItem() != Items.SPLASH_POTION
+                && stack.getItem() != Items.LINGERING_POTION && stack.getItem() != Items.TIPPED_ARROW) {
+            return;
+        }
+
+        List<MobEffectInstance> effects = net.minecraft.world.item.alchemy.PotionUtils.getMobEffects(stack);
+        MobEffectInstance burrowingInstance = null;
+        for (MobEffectInstance effect : effects) {
+            if (effect.getEffect() == ModEffects.BURROWING.get()) {
+                burrowingInstance = effect;
+                break;
+            }
+        }
+
+        if (burrowingInstance != null) {
+            // Set up lines
+            List<Component> tooltip = event.getToolTip();
+            String boostText = (burrowingInstance.getAmplifier() == 0) ? "50%" : "100%";
+            Component header = Component.translatable("potion.whenDrank").withStyle(ChatFormatting.DARK_PURPLE);
+            Component statLine = Component.translatable("tooltip.fungidelight.burrowing_desc", boostText)
+                    .withStyle(ChatFormatting.BLUE);
+
+            // Check if the "When Applied" header already exists
+            boolean headerExists = false;
+            for (Component component : tooltip) {
+                if (component.getString().equals(header.getString())) {
+                    headerExists = true;
+                    break;
+                }
+            }
+            // Add lines
+            if (!headerExists) {
+                tooltip.add(Component.empty());
+                tooltip.add(header);
+            }
+            tooltip.add(statLine);
+        }
+    }
+}
