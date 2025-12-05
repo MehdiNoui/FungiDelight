aace36fcfa0157dd6c3515f07d42791a326313b0
MehdiNoui <mehdinoui2004@gmail.com>
2025-12-05 23:41:53 +0100

    apply new effect on inky cap food items

--- src/main/java/net/mehdinoui/fungidelight/common/item/ModFoods.java
+++ src/main/java/net/mehdinoui/fungidelight/common/item/ModFoods.java
@@ -12,17 +12,22 @@ import static vectorwing.farmersdelight.common.FoodValues.LONG_DURATION;
 public class ModFoods {
     // BASIC INGREDIENTS & COMPONENTS
     public static final FoodProperties CLEANED_CAPS = new FoodProperties.Builder().
-            nutrition(2).saturationMod(0.2f).build();
+            nutrition(2).saturationMod(0.2f)
+            .effect(() -> new MobEffectInstance(net.mehdinoui.fungidelight.common.registry.ModEffects.WEAK_STOMACH.get(),BRIEF_DURATION , 0), 1.0F)
+            .build();
     public static final FoodProperties COOKED_CLEANED_CAPS = new FoodProperties.Builder().
             nutrition(4).saturationMod(0.4f).build();
     public static final FoodProperties TRUFFLE = new FoodProperties.Builder().
-            nutrition(4).saturationMod(0.4f).build();
+            nutrition(4).saturationMod(0.4f)
+            .effect(() -> new MobEffectInstance(net.mehdinoui.fungidelight.common.registry.ModEffects.WEAK_STOMACH.get(),BRIEF_DURATION , 0), 1.0F)
+            .build();
     public static final FoodProperties TRUFFLE_SLICE = new FoodProperties.Builder().
             nutrition(2).saturationMod(0.2f).build();
 
     // SALADS
     public static final FoodProperties INKY_CAPS_SALAD = new FoodProperties.Builder().
             nutrition(6).saturationMod(0.6f)
+            .effect(() -> new MobEffectInstance(net.mehdinoui.fungidelight.common.registry.ModEffects.WEAK_STOMACH.get(),BRIEF_DURATION , 0), 1.0F)
             .effect(() -> new MobEffectInstance(MobEffects.LUCK,100 , 0), 1.0F).build();
 
     // SNACKS & BAKED GOODS
@@ -40,6 +45,7 @@ public class ModFoods {
     // PLATED MEALS
     public static final FoodProperties INKY_CAP_SCRAMBLED_EGGS = new FoodProperties.Builder(). // Note: DANDELION_AND_EGGS in output.accept.
             nutrition(10).saturationMod(0.6f)
+            .effect(() -> new MobEffectInstance(net.mehdinoui.fungidelight.common.registry.ModEffects.WEAK_STOMACH.get(),BRIEF_DURATION , 0), 1.0F)
             .effect(() -> new MobEffectInstance(ModEffects.NOURISHMENT.get(), BRIEF_DURATION, 0), 1.0F)
             .build();
     public static final FoodProperties TRUFFLE_PASTA = new FoodProperties.Builder().
