045670af109ed397ad0fd8aa1b16a82cf13630db
MehdiNoui <mehdinoui2004@gmail.com>
2025-12-15 17:54:45 +0100

    change the effect category

--- src/main/java/net/mehdinoui/fungidelight/common/effect/WeakStomachEffect.java
+++ src/main/java/net/mehdinoui/fungidelight/common/effect/WeakStomachEffect.java
@@ -5,7 +5,7 @@ import net.minecraft.world.effect.MobEffectCategory;
 
 public class WeakStomachEffect extends MobEffect {
     public WeakStomachEffect() {
-        super(MobEffectCategory.BENEFICIAL, 0x4C5578);
+        super(MobEffectCategory.HARMFUL, 0x4C5578);
     }
     @Override
     public boolean isDurationEffectTick(int duration, int amplifier) {
